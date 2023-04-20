90082b5183d70b89a85e4e5a1684339897e18da3
Eleana Ortega <eleanaortega@outlook.com>
2023-04-19 22:55:12 -0400

    worked on user folders

--- server/config/mongoose.config.js
+++ server/config/mongoose.config.js
@@ -4,5 +4,5 @@ mongoose.connect('mongodb://127.0.0.1:27017/group_project', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
-    .then(() => console.log('Established a connection to the database expense report'))
+    .then(() => console.log('Established a connection to the database'))
     .catch(err => console.log('Something went wrong when connecting to the database ', err));
--- server/server.js
+++ server/server.js
@@ -13,8 +13,7 @@ app.use(cors({credentials: true, origin: 'http://localhost:3000'}));
 app.use(express.json(), express.urlencoded({ extended: true }));
 
 require('./routes/user.routes')(app);
-require('./routes/restaurant.routes')(app);
-require('./routes/review.routes')(app);
+
 
 
 app.listen(port, () => console.log(`Listening on port: ${port}`));
