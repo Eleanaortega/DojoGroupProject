a1324b67a037bc55441d09c8e301b6d1fad1204a
Eleana Ortega <eleanaortega@outlook.com>
2023-04-21 14:20:02 -0400

    changes to server.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,41 +1,12 @@
 import React from 'react'
-import io from 'socket.io-client';
-import { useState, useEffect } from 'react';
-const css = require('/Users/eleanaortega/Desktop/DojoGroupProject/client/src/chat.css')
-
+import Chats from './components/Chats.jsx'
+import {Routes, Route} from 'react-router-dom';
 
 const App = () => {
-
-  const [socket] = useState(() => io(':8000'));
-
-
-    const handleSubmit = (e) => {
-      e.preventDefault();
-      const input = document.getElementById('input');
-      if (input.value) {
-        socket.emit('chat message', input.value);
-        input.value = '';
-      }
-      
-      socket.on('chat message', function(msg) {
-        const messages = document.getElementById('messages');
-        var item = document.createElement('li');
-        item.textContent = msg;
-        messages.appendChild(item);
-        window.scrollTo(0, document.body.scrollHeight);
-
-      })
-    }
-        return (
+  return (
     <div>
-      <body>
-        <ul id="messages"></ul>
-          <form id="form" onSubmit={handleSubmit}>
-            <input id="input" autoComplete="off" />
-            <button type="submit">Send</button>
-          </form>
-          
-      </body>
+      <h1>Test</h1>
+      <Chats/>
     </div>
   )
 }
