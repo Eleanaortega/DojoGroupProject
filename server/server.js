c518b6d5b9249ac26ec1fe3941e88fd03d779964
Eleana Ortega <eleanaortega@outlook.com>
2023-04-20 23:04:12 -0400

    trying out socket.io

--- server/server.js
+++ server/server.js
@@ -3,19 +3,46 @@ const app = express();
 const port = 8000;
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
+const socket = require('socket.io');
+const http = require('http');
+
 
 require('dotenv').config();
-
 require('./config/mongoose.config');
+require('./routes/user.routes')(app);
+require('./routes/message.routes')(app);
+
 
 app.use(cookieParser());
 app.use(cors({credentials: true, origin: 'http://localhost:3000'}));
 app.use(express.json(), express.urlencoded({ extended: true }));
 
+const server = app.listen(port, () => {
+    console.log(`Listening on port: ${port}`)
+});
 
-require('./routes/user.routes')(app);
-require('./routes/message.routes')(app);
+const io = socket(server, {
+    cors: {
+        origin: 'http://localhost:3000',
+        methods: ['GET', 'POST'],
+        allowedHeaders: ['*'],
+        credentials: true,
+    }
+});
 
+app.get('/', (req, res) => {
+    res.sendFile('/Users/eleanaortega/Desktop/DojoGroupProject/client/src/App.js');
+    });
 
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+    });
 
-app.listen(port, () => console.log(`Listening on port: ${port}`));
+io.on('connection', (socket) => {
+    socket.on('chat message', (msg) => {
+        console.log('message: ' + msg);
+    });
+    });
