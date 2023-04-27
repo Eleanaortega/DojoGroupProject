512bce1027155da2e3082969c92cb4a05f3ab650
Roger Baptiste <rogerbap@outlook.com>
2023-04-26 22:22:33 -0500

    update

--- server/controllers/message.controllers.js
+++ server/controllers/message.controllers.js
@@ -1,57 +1,65 @@
 const Message = require("../models/message.models");
 const jwt = require("jsonwebtoken");
+const asyncHandler = require("express-async-handler");
+const mongoose = require('mongoose')
 const secret = process.env.SECRET_KEY;
 
 // FIND ALL MESSAGES
 module.exports.findAllMessages = (req, res) => {
-    Message.find()
-        .then(allMessages => res.json({ messages: allMessages}))
-        .catch(err => res.json({ message: "Something went wrong", error: err }));
+  Message.find()
+    .then(allMessages => res.json({ messages: allMessages }))
+    .catch(err => res.json({ message: "Something went wrong", error: err }));
 }
 
 // FIND A MESSAGE
 module.exports.findOneMessage = (req, res) => {
-    Message.findById(req.params.id)
-        .then(oneMessage => res.json({ message: oneMessage}))
-        .catch(err => res.json({ message: "Something went wrong", error: err }));
+  Message.findById(req.params.id)
+    .then(oneMessage => res.json({ message: oneMessage }))
+    .catch(err => res.json({ message: "Something went wrong", error: err }));
 }
 
 // Create Message Two
 const sendMessage = asyncHandler(async (req, res) => {
-    const { content, chatId } = req.body;
+  const { content, chatId } = req.body;
+
+  const { userId } = req.body;
+  const decodedToken = jwt.verify(req.cookies.usertoken, secret);
+  console.log("token from body:", decodedToken._id)
+  const loggedInUserId = decodedToken._id
+  console.log("does this work", loggedInUserId);
+
+  if (!content || !chatId) {
+    console.log("Invalid data passed into request");
+    return res.sendStatus(400);
+  }
   
-    if (!content || !chatId) {
-      console.log("Invalid data passed into request");
-      return res.sendStatus(400);
-    }
-  
-    var newMessage = {
-      sender: req.user._id,
-      content: content,
-      chat: chatId,
-    };
-  
-    try {
-      var message = await Message.create(newMessage);
-  
-      message = await message.populate("sender", "firstName picture").execPopulate();
-      message = await message.populate("chat").execPopulate();
-      message = await User.populate(message, {
-        path: "chat.users",
-        select: "firstName picture email",
-      });
-  
-      await Chat.findByIdAndUpdate(req.body.chatId, { lastMessage: message });
-  
-      res.json(message);
-    } catch (error) {
-      res.status(400);
-      throw new Error(error.message);
-    }
-  });
-  
-  module.exports = { sendMessage };
-  
+  var newMessage = {
+    sender: loggedInUserId,
+    content: content,
+    chat: chatId,
+  };
+
+  try {
+    var message = await Message.create(newMessage);
+
+    message = await message.populate("sender", "firstName", "picture")
+    message = await message.populate("chat")
+    message = await User.populate(message, {
+      path: "chat.users",
+      select: "firstName picture email",
+    });
+
+    await Chat.findByIdAndUpdate(req.body.chatId, { lastMessage: message });
+
+    res.json(message);
+  } catch (error) {
+    res.status(400);
+    // throw new Error(error.message);
+  }
+});
+
+module.exports = { sendMessage };
+
 
 // // CREATE MESSAGE
 // module.exports.createMessage = async (req, res) => {
@@ -67,7 +75,7 @@ const sendMessage = asyncHandler(async (req, res) => {
 //     }
 
 //     console.log("User", loggedInUserId)
-    
+
 //     var newMessage = {
 //         sender: loggedInUserId,
 //         content: content,
@@ -95,14 +103,14 @@ const sendMessage = asyncHandler(async (req, res) => {
 
 // UPDATE MESSAGE
 module.exports.updateMessage = (req, res) => {
-    Message.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
-        .then(updatedMessage => res.json({ Message: updatedMessage }))
-        .catch(err => res.json({ message: "Something went wrong", error: err }));
+  Message.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
+    .then(updatedMessage => res.json({ Message: updatedMessage }))
+    .catch(err => res.json({ message: "Something went wrong", error: err }));
 }
 
 // DELETE MESSAGE
 module.exports.deleteMessage = (req, res) => {
-    Message.findByIdAndDelete(req.params.id)
-        .then(result => res.json({ result: result }))
-        .catch(err => res.json({ message: "Something went wrong", error: err }));
+  Message.findByIdAndDelete(req.params.id)
+    .then(result => res.json({ result: result }))
+    .catch(err => res.json({ message: "Something went wrong", error: err }));
 }
