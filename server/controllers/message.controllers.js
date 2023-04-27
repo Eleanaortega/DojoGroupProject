aa7f6fc555ae6f69852667990e9c10c692ebe86d
japhethbear <japhethbear@gmail.com>
2023-04-26 21:37:33 -0500

    Added message controller

--- server/controllers/message.controllers.js
+++ server/controllers/message.controllers.js
@@ -1,4 +1,6 @@
 const Message = require("../models/message.models");
+const jwt = require("jsonwebtoken");
+const secret = process.env.SECRET_KEY;
 
 // FIND ALL MESSAGES
 module.exports.findAllMessages = (req, res) => {
@@ -14,13 +16,82 @@ module.exports.findOneMessage = (req, res) => {
         .catch(err => res.json({ message: "Something went wrong", error: err }));
 }
 
-// CREATE MESSAGE
-module.exports.createMessage = (req, res) => {
-    console.log(req.body)
-    Message.create(req.body)
-        .then(newMessage => res.json({ message: newMessage }))
-        .catch(err => res.json({ message: "Something went wrong", error: err }));
-}
+// Create Message Two
+const sendMessage = asyncHandler(async (req, res) => {
+    const { content, chatId } = req.body;
+  
+    if (!content || !chatId) {
+      console.log("Invalid data passed into request");
+      return res.sendStatus(400);
+    }
+  
+    var newMessage = {
+      sender: req.user._id,
+      content: content,
+      chat: chatId,
+    };
+  
+    try {
+      var message = await Message.create(newMessage);
+  
+      message = await message.populate("sender", "firstName picture").execPopulate();
+      message = await message.populate("chat").execPopulate();
+      message = await User.populate(message, {
+        path: "chat.users",
+        select: "firstName picture email",
+      });
+  
+      await Chat.findByIdAndUpdate(req.body.chatId, { lastMessage: message });
+  
+      res.json(message);
+    } catch (error) {
+      res.status(400);
+      throw new Error(error.message);
+    }
+  });
+  
+  module.exports = { sendMessage };
+  
+
+// // CREATE MESSAGE
+// module.exports.createMessage = async (req, res) => {
+//     const { content, chatId } = req.body;
+//     const { userId } = req.body;
+//     const decodedToken = jwt.verify(req.cookies.usertoken, secret);
+//     console.log("token from body:", decodedToken._id)
+//     const loggedInUserId = decodedToken._id
+//     console.log(req.body)
+//     if (!content || !chatId) {
+//         console.log("Invalid data passed into request");
+//         return res.sendStatus(400);
+//     }
+
+//     console.log("User", loggedInUserId)
+    
+//     var newMessage = {
+//         sender: loggedInUserId,
+//         content: content,
+//         chat: chatId,
+//     };
+
+//     console.log("UserTwo", req.user)
+
+//     var message = await Message.create(newMessage)
+//         .then(newMessage => {
+//             message = message.populate('sender', 'firstName', 'lastName', 'picture').execPopulate();
+//             message = message.populate('chat').execPopulate();
+//             message = User.populate(message, {
+//                 path: 'chat.users',
+//                 select: 'firstName picture email'
+//             });
+
+//             Chat.findByIdAndUpdate(req.body.chatId), {
+//                 lastMessage: message,
+//             }
+//                 res.json(message);
+//             })
+//         .catch(err => res.json({ message: "Something went wrong", error: err }));
+// }
 
 // UPDATE MESSAGE
 module.exports.updateMessage = (req, res) => {
--- server/routes/message.routes.js
+++ server/routes/message.routes.js
@@ -1,9 +1,11 @@
 const MessageController = require('../controllers/message.controllers');
+const { authenticate } = require('../config/jwt.config')
+
 
 module.exports = app => {
-    app.get('/api/messages', MessageController.findAllMessages);
+    app.get('/api/messages', authenticate, MessageController.findAllMessages);
     app.get('/api/messages/:id', MessageController.findOneMessage);
-    app.post('/api/messages', MessageController.createMessage);
+    app.post('/api/messages', authenticate, MessageController.createMessage);
     app.put('/api/messages/:id', MessageController.updateMessage);
 
     // app.patch('/api/messages/:id', MessageController.updateMessage);
